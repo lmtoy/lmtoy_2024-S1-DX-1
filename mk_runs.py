eb71bcf55c1812153d269fbc6358cb672d3b2192
Peter Teuben <teuben@gmail.com>
2025-07-07 19:26:07 +0000

    added new data

--- mk_runs.py
+++ mk_runs.py
@@ -17,18 +17,28 @@ on["PG1202+281"] = \
 on["PG1402+261"] = \
  [ 131808, 131809, 131810, 131812,-131813,]
 
+on["PG1425+267"] = \
+ [ 136910, 136911, 136912, 136914, 136915, 136916, 136918, 136919, 136920,
+   138020, 138021, 138022, 138024, 138025, 138026, 138028, 138029,]
+
+on["PG1427+480"] = \
+ [ 136925, 136926, 136927,]
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
 pars1["PG1202+281"] = ""
 pars1["PG1402+261"] = ""
-
+pars1["PG1425+267"] = ""
+pars1["PG1427+480"] = ""
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
 pars2["PG1202+281"] = ""
 pars2["PG1402+261"] = ""
+pars2["PG1425+267"] = ""
+pars2["PG1427+480"] = ""
+
 
 
 if __name__ == '__main__':    
