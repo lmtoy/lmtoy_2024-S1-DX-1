61b93dc5f3f423dcf8380e7d93200e1a2cf2a791
lkbajaj <mwatstein@umass.edu>
2025-08-03 21:44:13 +0000

    Aug 2nd QA

--- mk_runs.py
+++ mk_runs.py
@@ -18,20 +18,17 @@ on["PG1402+261"] = \
  [ 131808, 131809, 131810, 131812,-131813,]
 
 on["PG1425+267"] = \
- [ 136910, 136911, 136912, 136914, 136915, 136916, 136918, 136919, 136920,
-   138020, 138021, 138022, 138024, 138025, 138026, 138028, 138029,
-   138202, 138203, 138204, 138206, 138207, 138208, 138419, 138420, 138421,]
+ [ 138020, 138021, 138022, 138024, 138025, 138026, 138028, 138029, 138202, 138203, 138204, 138206, 138207, 138208, 138419, 138420, 138421,]
 
 on["PG1427+480"] = \
  [ 136925, 136926, 136927,]
 
-
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["PG1202+281"] = ""
-pars1["PG1402+261"] = ""
-pars1["PG1425+267"] = ""
-pars1["PG1427+480"] = ""
+pars1["PG1202+281"] = "qagrade=3 speczoom=80,3"
+pars1["PG1402+261"] = "qagrade=3"
+pars1["PG1425+267"] = "qagrade=3 speczoom=80,3"
+pars1["PG1427+480"] = "qagrade=3"
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
@@ -43,4 +40,4 @@ pars2["PG1427+480"] = ""
 
 
 if __name__ == '__main__':    
-    runs.mk_runs(project, on, pars1, pars2, None, sys.argv)
+    runs.mk_runs(project, on, pars1, pars2, sys.argv)
